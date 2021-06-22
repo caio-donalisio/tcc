3297ebb9635505a5781e7887e94395a7ccb52168
Thiago Moretto <thiago.moretto@gmail.com>
2021-06-22 17:46:43 -0300

    Add google storage support

--- storage.py
+++ storage.py
@@ -2,6 +2,26 @@ from pathlib import Path
 from urllib.parse import urlparse
 
 
+import google.cloud.storage as gstorage
+
+storage_client = None
+buckets = {}
+
+
+def get_storage_client():
+  global storage_client
+  if storage_client is None:
+    storage_client = gstorage.Client()
+  return storage_client
+
+
+def get_bucket_ref(bucket_name):
+  bucket = buckets.get(bucket_name)
+  if not bucket:
+    buckets[bucket_name] = get_storage_client().bucket(bucket_name)
+  return buckets[bucket_name]
+
+
 def store(path, contents):
   urlparts = urlparse(path)
   if urlparts.scheme == 'gs':
@@ -11,8 +31,12 @@ def store(path, contents):
 
 
 def store_on_gs(path, contents):
-  # TODO: implement
-  pass
+  urlparts = urlparse(path)
+  bucket_name = urlparts.netloc
+  blob_name   = urlparts.path.lstrip('/')
+  bucket = get_bucket_ref(bucket_name)
+  blob = bucket.blob(blob_name)
+  blob.upload_from_string(contents)
 
 
 def store_on_fs(path, contents):
