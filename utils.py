4aae1228403ef34d1cb37fca9d31f4946b4ca962
Thiago Moretto <thiago.moretto@gmail.com>
2021-06-30 10:26:21 -0300

    Add  model and improve 'weekly/montly/timely' generators

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 import os
+import json
 import logging
 import urllib.request
 import time
 import http.client
 import requests
+import itertools
+import hashlib
 import io
 from tqdm import tqdm
 from datetime import datetime
@@ -14,11 +17,17 @@ from functools import wraps
 import wrapt
 from urllib.parse import urlencode
 
+from storage import (get_bucket_ref, )
+
 
 class GSOutput:
-    def __init__(self, bucket_name, client=None):
-        self._client = (client or storage.Client())
-        self._bucket = self._client.get_bucket(bucket_name)
+    def __init__(self, bucket_name):
+        self._bucket_name = bucket_name
+        self._bucket = get_bucket_ref(bucket_name)
+
+    def exists(self, filepath):
+        blob = self._bucket.blob(filepath)
+        return blob.exists()
 
     def save_from_contents(self, filepath, contents, **kwargs):
         blob = self._bucket.blob(filepath)
@@ -30,6 +39,10 @@ class GSOutput:
         if blob.exists():
             return blob.download_as_bytes()
 
+    @property
+    def uri(self):
+        return f'gs://{self._bucket_name}'
+
     def __repr__(self):
         return f"GS({self._bucket})"
 
@@ -37,6 +50,10 @@ class GSOutput:
 class LSOutput:
     def __init__(self, output_folder):
         self._output_folder = output_folder
+
+    def exists(self, filepath):
+        source = f'{self._output_folder}/{filepath}'
+        return Path(source).exists()
 
     def save_from_contents(self, filepath, contents, **kwargs):
         target = f'{self._output_folder}/{filepath}'
@@ -47,6 +64,10 @@ class LSOutput:
         if Path(source).exists():
             with open(source, 'r') as f:
                 return f.read()
+
+    @property
+    def uri(self):
+        return self._output_folder
 
     def __repr__(self):
         return f"Local({self._output_folder})"
@@ -172,3 +193,76 @@ class TqdmToLogger(io.StringIO):
 
     def flush(self):
         self.logger.log(self.level, self.buf)
+
+
+def pairwise(iterable):
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def monthly(start_date, end_date):
+    date_range = start_date.diff(end_date)
+    if date_range.in_months() > 0:
+        yield from timely(start_date, end_date, unit='months', step=1)
+    else:
+        yield start_date, end_date
+
+
+def weekly(start_date, end_date):
+    date_range = start_date.diff(end_date)
+    if date_range.in_weeks() > 0:
+        yield from timely(start_date, end_date, unit='weeks', step=1)
+    else:
+        yield start_date, end_date
+
+
+def timely(start_date, end_date, unit, step):
+    date_range = start_date.diff(end_date)
+    dates = list(date_range.range(unit, step))
+    if end_date > dates[-1]:
+        dates.append(end_date)
+    pairs = list(pairwise(dates))
+    for x, y in pairs[:-1]:
+        yield x, y.subtract(days=1)
+    yield pairs[-1]
+
+
+class Chunk:
+  def __init__(self, params, output, rows_generator):
+    self._params = params
+    self._output = output
+    self._rows_generator = rows_generator
+    self._values = {**{}, **params}
+    if self.commited():
+      self._restore_values()
+
+  def _restore_values(self):
+    raw_value = self._output.load_as_string(self.filepath)
+    self._values = json.loads(raw_value)
+
+  @property
+  def filepath(self):
+    return f'.state/{self.hash}.json'
+
+  def commit(self):
+    self._output.save_from_contents(
+      filepath=self.filepath, contents=json.dumps(self._values),
+      content_type='application/json')
+
+  def commited(self):
+    return self._output.exists(self.filepath)
+
+  def set_value(self, key, value):
+    self._values[key] = value
+
+  def get_value(self, key):
+    return self._values[key]
+
+  @property
+  def hash(self):
+    return hashlib.sha1(repr(sorted(self._params.items())).encode()) \
+      .hexdigest()
+
+  def rows(self):
+    yield from self._rows_generator
