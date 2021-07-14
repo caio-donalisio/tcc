33e5ddbfac23809bd59c7b7e99ca40e5c83051b9
Thiago Moretto <thiago.moretto@gmail.com>
2021-07-14 12:02:35 -0300

    Disable celery prefetching (use ack late as well) and increase visibility timeout
    We are setting this to avoid the behavior of retry loop we are having
    for long running tasks.

---
+++ celeryconf.py
@@ -0,0 +1,3 @@
+task_acks_late = True
+worker_prefetch_multiplier = 1
+result_backend_transport_options = {'visibility_timeout': 3600 * 2}  # 2 hours
--- start-celery.sh
+++ start-celery.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-CELERYD_HIJACK_ROOT_LOGGER=false celery -A tasks worker -Q persistence,crawlers,downloader --concurrency=2 -E --loglevel=INFO --detach;
+CELERYD_HIJACK_ROOT_LOGGER=false celery --config=celeryconf -A tasks worker -Q persistence,crawlers,downloader --concurrency=2 -E --loglevel=INFO --detach;
