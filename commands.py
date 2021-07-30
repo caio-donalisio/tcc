5f869cb118eac16967f15aa8ad6368d90b8c2b51
Thiago Moretto <thiago.moretto@gmail.com>
2021-07-30 12:12:36 -0300

    Add useful command to clear celery_singleton locks

--- commands.py
+++ commands.py
@@ -18,5 +18,13 @@ from crawlers.tjba.tjba_crawler import (
 from crawlers.tjsp2.tjsp_pdf import tjsp_pdf_command
 
 
+@cli.command(name='clear-locks')
+def clear_celery_singleton_locks():
+  from celery_singleton import clear_locks
+  from app import celery
+  clear_locks(celery)
+  print('OK')
+
+
 if __name__ == '__main__':
   cli(obj={})  # type: ignore
