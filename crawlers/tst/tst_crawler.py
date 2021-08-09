cb61f5b147147347221a1302a3fa73c255895845
Caio Donalisio <caio.donalisio@gmail.com>
2021-08-08 23:07:52 -0300

    string Formatting

--- crawlers/tst/tst_crawler.py
+++ crawlers/tst/tst_crawler.py
@@ -18,7 +18,10 @@ class TSTClient:
 
     def __init__(self):
         self.url ='https://jurisprudencia-backend.tst.jus.br/rest/pesquisa-textual'
-        self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67'
+        self.user_agent = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                        ' AppleWebKit/537.36 (KHTML, like Gecko)'
+                        ' Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67')
+
 
     @utils.retryable(max_retries=3)
     def count(self,filters):
