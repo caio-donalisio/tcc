9aacfd6ba2bd7f5d1813c7c426ee8dfebe271eef
Caio Donalisio <caio.donalisio@gmail.com>
2021-09-01 14:53:24 -0300

    Functioning crawler

--- crawlers/trf3/trf3_crawler.py
+++ crawlers/trf3/trf3_crawler.py
@@ -15,10 +15,12 @@ from bs4 import BeautifulSoup
 
 
 DEFAULT_HEADERS = {
-  'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-                        ' AppleWebKit/537.36 (KHTML, like Gecko)'
-                        ' Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67')
-}
+     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
+     'Accept': '*/*',
+     'Accept-Encoding':'gzip, deflate',
+     'Referer': 'http://web.trf3.jus.br/base-textual'
+            }
+
 DEFAULT_DATE_FORMAT = 'YYYY-MM-DD'
 TRF3_DATE_FORMAT = 'DD/MM/YYYY'
 
@@ -28,26 +30,34 @@ logger = logger_factory('trf3')
 class TRF3Client:
 
     def __init__(self):
+
+        self.session = requests.Session()#.get('http://web.trf3.jus.br/base-textual',headers=DEFAULT_HEADERS)
         #self.url ='https://jurisprudencia-backend.trf3.jus.br/rest/pesquisa-textual'
-        self.url = 'http://web.trf3.jus.br/base-textual/Home/ResultadoTotais'
+        #self.url = 
+
+    def setup(self):
+        self.session.get('http://web.trf3.jus.br/base-textual',headers=DEFAULT_HEADERS)
 
     @utils.retryable(max_retries=9)
     def count(self,filters):
-        result = self.fetch(filters,page=1)
-        soup = BeautifulSoup(result)
-        text = soup.find('a',{'href':'/base-textual/Home/ListaResumida/1?np=0'}).text
-        print(text)
-        return result['totalRegistros']
+        result = self.fetch(filters)
+        soup = BeautifulSoup(result.text)
+        count = soup.find('a',{'href':'/base-textual/Home/ListaResumida/1?np=0'}).text
+        if count:
+            return int(''.join([char for char in count if char.isdigit()]))
+        else:
+            return 0
 
     @utils.retryable(max_retries=9)
-    def fetch(self, filters, page=1):
+    def fetch(self, filters):
+        self.setup()
         post_data = {
         'txtPesquisaLivre': '',
         'chkMostrarLista': 'on',
         'numero': '',
         'magistrado': 0,
-        'data_inicial': filters.get('start_date').format(TRF3_DATE_FORMAT),
-        'data_final': filters.get('end_date').format(TRF3_DATE_FORMAT),
+        'data_inicial': pendulum.parse(filters.get('start_date')).format(TRF3_DATE_FORMAT),
+        'data_final': pendulum.parse(filters.get('end_date')).format(TRF3_DATE_FORMAT),
         'data_tipo': 1,
         'classe': 0,
         'numclasse': '',
@@ -62,9 +72,10 @@ class TRF3Client:
         'hdnLegislacao': '',
         'hdnMostrarListaResumida': ''
     }
+        url = 'http://web.trf3.jus.br/base-textual/Home/ResultadoTotais'
 
         #sleep(0.5*random())
-        return requests.post(self.url,
+        return self.session.post(url,
             json=post_data,
             headers=DEFAULT_HEADERS)
 
@@ -72,7 +83,7 @@ class TRF3Client:
 
 class TRF3Collector(base.ICollector):
 
-    def __init__(self, client, filters):
+    def __init__(self, client,filters):
         self.client = client
         self.filters = filters
 
@@ -81,35 +92,54 @@ class TRF3Collector(base.ICollector):
 
     def chunks(self):
         total = self.count()
-        pages = math.ceil(total/10)
-
     
-        for page in range(1, pages + 1):
+        for proc_number in range(1, total + 1):
             yield TRF3Chunk(
-                keys={**self.filters , **{'page': page}},
+                keys={**self.filters , **{'proc_number': proc_number}},
                 prefix='',
-                filters=self.filters,
-                page=page,
-                client=self.client
+                #filters=self.filters,
+                proc_number=proc_number,
+                #session = self.session,
+                client=self.client,
+                
             )
 
 class TRF3Chunk(base.Chunk):
 
-    def __init__(self, keys, prefix, filters, page,client):
+    def __init__(self, keys, prefix, proc_number,client):
         super(TRF3Chunk, self).__init__(keys, prefix)
-        self.filters  = filters
-        self.page = page
+        #self.filters  = filters
+        self.proc_number = proc_number
+        #self.session = session
         self.client = client
+        
 
     def rows(self):
-        result = self.client.fetch(self.filters,self.page)
+        
+        response = self.client.session.get(f'http://web.trf3.jus.br/base-textual/Home/ListaColecao/9?np={self.proc_number}',headers=DEFAULT_HEADERS)
+        soup = BeautifulSoup(response.text)
+        
+        data_julg_div = soup.find('div',text='Data do Julgamento ')
+        session_at = data_julg_div.next_sibling.next_sibling.text.strip()
+        session_at = pendulum.from_format(session_at,TRF3_DATE_FORMAT)
 
-        for record in result['registros']:
+        processo_div = soup.find('h4',text='Processo').next_sibling.next_sibling.text.strip()
+        processo_num = ''.join(char for char in processo_div if char.isdigit())
 
-            sleep(random()+1.13)
+        dest_path = f'{session_at.year}/{session_at.month:02d}/{session_at.day:02d}_{processo_num}.html'
+
+        #result = self.client.fetch(self.filters,self.page)
+
+        #for record in result['registros']:
+
+        #    sleep(random()+1.13)
 
 
-            yield base.Content('aaa','05/05/bla.html')
+        yield [base.Content(
+            content=response.text,
+            dest=dest_path,
+            content_type='text/html')]
+
 
 
 
@@ -126,8 +156,8 @@ def trf3_task(**kwargs):
         logger.info(f'Output: {output}.')
 
         query_params = {
-            'start_date':pendulum.parse(kwargs.get('start_date')),
-            'end_date':pendulum.parse(kwargs.get('end_date'))
+            'start_date':kwargs.get('start_date'),
+            'end_date':kwargs.get('end_date')
             }
 
         collector = TRF3Collector(client=TRF3Client(), filters=query_params)
