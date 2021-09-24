07cad3ace5662396fe2ccd5865d79abed55093aa
Thiago Moretto <thiago.moretto@gmail.com>
2021-09-24 18:00:51 -0300

    TJRJ: on certain cases ignore instead of waiting almost indefinitely

--- crawlers/tjrj/tjrj_crawler.py
+++ crawlers/tjrj/tjrj_crawler.py
@@ -59,7 +59,7 @@ class TJRJ(base.BaseCrawler, base.ICollector):
       'grpEssj': ''
     }
 
-  @utils.retryable(max_retries=99)
+  @utils.retryable(max_retries=9)
   def count(self):
     url = f'{EJURIS_URL}/ProcessarConsJuris.aspx/ExecutarConsultarJurisprudencia'
     logger.debug(f'POST (count) {url}')
@@ -130,14 +130,14 @@ class TJRJ(base.BaseCrawler, base.ICollector):
 
 class TJRJHandler(base.ContentHandler):
 
-  @utils.retryable(max_retries=99)
+  @utils.retryable(max_retries=3, ignore_if_exceeds=True)
   def handle(self, event):
     if isinstance(event, base.ContentFromURL):
       self.download(event)
     else:
       super().handle(event)
 
-  @utils.retryable(max_retries=99, sleeptime=5., ignore_if_exceeds=True)   # type: ignore
+  @utils.retryable(max_retries=3, sleeptime=5., ignore_if_exceeds=True)   # type: ignore
   def download(self, content_from_url):
     if self.output.exists(content_from_url.dest):
       return
@@ -220,7 +220,7 @@ class TJRJChunk(base.Chunk):
       ]
       time.sleep(random.uniform(.05, .20))
 
-  @utils.retryable(max_retries=99)
+  @utils.retryable(max_retries=9)
   def _fetch_data(self, act, headers, act_id, updated_at, fetch_all_pdfs=False):
     data_url = f'{EJUD_URL}/ConsultaEjud.asmx/DadosProcesso_1'
     data_payload = {
@@ -266,7 +266,7 @@ class TJRJChunk(base.Chunk):
       *extra_contents
     ]
 
-  @utils.retryable(max_retries=99)
+  @utils.retryable(max_retries=9)
   def _get_acts(self, page, payload, headers):
     payload['numPagina'] = page
     url = f'{EJURIS_URL}/ProcessarConsJuris.aspx/ExecutarConsultarJurisprudencia'
