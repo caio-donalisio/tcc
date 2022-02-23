48f71ab3a79cc7f4a3e6d90fe57930bc12b8349b
Ricardo Verhaeg <ricardo.verhaeg@inspira.legal>
2022-02-23 19:10:20 +0000

    feat: Enable event exporter to be used by metrics

--- celeryconf.py
+++ celeryconf.py
@@ -2,3 +2,6 @@ task_acks_late = True
 worker_prefetch_multiplier = 1
 result_backend_transport_options = {'visibility_timeout': 3600 * 2}  # 2 hours
 broker_transport_options = {'visibility_timeout': 3600 * 2}
+# 
+worker_send_task_events = True
+task_send_sent_event = True
