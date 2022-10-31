b726a719894419d1ba3ddae82831e9b1e0a9e067
Ricardo Verhaeg <ricardo.verhaeg@inspira.legal>
2022-10-31 17:46:34 -0300

    fix: Changes TJRS default from beggining of year to three months
    Signed-off-by: Ricardo Verhaeg <ricardo.verhaeg@inspira.legal>

--- crawlers/tjrs/tjrs_crawler.py
+++ crawlers/tjrs/tjrs_crawler.py
@@ -224,7 +224,7 @@ def tjrs_task(**kwargs):
 
 @cli.command(name='tjrs')
 @click.option('--start-date',
-  default=utils.DefaultDates.BEGINNING_OF_YEAR_OR_SIX_MONTHS_BACK.strftime("%Y-%m-%d"),
+  default=utils.DefaultDates.THREE_MONTHS_BACK.strftime("%Y-%m-%d"),
   help='Format YYYY-MM-DD.',
 )
 @click.option('--end-date'  ,
