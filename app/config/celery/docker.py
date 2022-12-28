17924842c0a525cba57ab733a6d625f173ed7e83
Anderson Caxias <anderson.silva@inspira.legal>
2022-12-28 13:36:51 -0300

    fix: add production config file

--- app/config/celery/docker.py
+++ app/config/celery/docker.py
@@ -1,7 +1,9 @@
 from .default import *  # noqa
 
-from celery.schedules import crontab
 from kombu import Queue
+
+TJSP_USERNAME = os.getenv("TJSP_USERNAME", None)
+TJSP_PASSWORD = os.getenv("TJSP_PASSWORD", None)
 
 result_backend = os.getenv("CELERY_BACKEND_URL", "redis://127.0.0.1:6379/0")
 broker_url = os.getenv("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
@@ -30,34 +32,12 @@ task_store_errors_even_if_ignored = True
 
 imports = ("app.crawlers",)
 
-crawler_tasks = [
-    "crawlers.carf", "crawlers.stf", "crawlers.stj", "crawlers.titsp", "crawlers.tjba", "crawlers.tjmg",
-    "crawlers.tjrs", "crawlers.tjsp", "crawlers.trf2", "crawlers.trf3", "crawlers.trf4",
-    "crawlers.tst"]
-
 task_queues = (
     Queue("crawler-queue", routing_key="crawlers.#"),
     Queue("schedule-queue", routing_key="schedule.#"),
 )
 
 beat_schedule = {}
-
-SCHEDULE_ENABLED = bool(os.getenv("SCHEDULE_ENABLED", False))
-
-SCHEDULE_BACKING_DAYS = int(os.getenv("SCHEDULE_BACKING_DAYS", 1))
-SCHEDULE_HOUR = int(os.getenv("SCHEDULE_HOUR", 23))
-SCHEDULE_MIN = int(os.getenv("SCHEDULE_MIN", 53))
-
-if SCHEDULE_ENABLED:
-    print("everyday_check_court_updates ok")
-    beat_schedule["everyday_check_court_updates"] \
-        = {
-        "task": "schedule.check_court_updates",
-        "schedule": crontab(minute=SCHEDULE_MIN, hour=SCHEDULE_HOUR,
-                            day_of_week='*',
-                            day_of_month='*', month_of_year='*'),
-        "options": {"queue": "schedule-queue"},
-    },
 
 timezone = "UTC"
 
