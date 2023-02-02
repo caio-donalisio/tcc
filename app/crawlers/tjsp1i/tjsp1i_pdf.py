9e892000a02f6877c08db1e7bee9c86d9fd92fd4
Caio Donalisio <caio.donalisio@inspira.legal>
2023-02-02 08:20:53 -0300

    fix: pdf download in batches and max-workers arg

--- app/crawlers/tjsp1i/tjsp1i_pdf.py
+++ app/crawlers/tjsp1i/tjsp1i_pdf.py
@@ -22,8 +22,6 @@ from selenium.webdriver.common.by import By
 
 logger = logger_factory('tjsp1i-pdf')
 
-MAX_WORKERS=15
-
 class TJSP1IDownloader:
 
   def __init__(self, output):
@@ -73,8 +71,8 @@ class TJSP1IDownloader:
     search_url = f"https://esaj.tjsp.jus.br/cjpg/obterArquivo.do?cdProcesso={kvs['cdProcesso']}&cdForo={kvs['cdForo']}&nmAlias={kvs['nmAlias']}&cdDocumento={kvs['cdDoc']}"
     browser.driver.execute_script(f"window.open('{search_url}')")
     browser.switch_to_window(1)
-    WebDriverWait(browser.driver, 20).until(EC.presence_of_element_located((By.XPATH, '//iframe[@src!="processando.html"]')))
-    browser.driver.implicitly_wait(20)
+    WebDriverWait(browser.driver, 30).until(EC.presence_of_element_located((By.XPATH, '//iframe[@src!="processando.html"]')))
+    browser.driver.implicitly_wait(30)
     iframe = browser.bsoup().find('iframe')
     relative_url = re.match(r'.*viewer\.html\?file=(.*)$', iframe['src'])
     if relative_url is None:
@@ -146,8 +144,12 @@ def tjsp1i_download(items, output_uri, pbar):
 @click.option('--dry-run'     , default=False, is_flag=True)
 @click.option('--local'       , default=False, is_flag=True)
 @click.option('--count'       , default=False, is_flag=True)
-def tjsp1i_pdf_command(input_uri, start_date, end_date, dry_run, local, count):
-  batch  = []
+@click.option('--max-workers' , default=7)
+@click.option('--batch' , default=100)
+def tjsp1i_pdf_command(input_uri, start_date, end_date, dry_run, local, count, max_workers, batch):
+  # batch  = []
+  global MAX_WORKERS
+  MAX_WORKERS=int(max_workers)
   output = utils.get_output_strategy_by_path(path=input_uri)
   startDate = pendulum.parse(start_date)
   endDate = pendulum.parse(end_date)
@@ -164,8 +166,12 @@ def tjsp1i_pdf_command(input_uri, start_date, end_date, dry_run, local, count):
   while startDate <= endDate:
     print(f"TJSP1I - Collecting {startDate.format('YYYY/MM')}...")
     pendings = []
+    counter = 0 
     for pending in list_pending_pdfs(output._bucket_name, startDate.format('YYYY/MM')):
       pendings.append(pending)
-
-    utils.run_pending_tasks(tjsp1i_download, pendings, input_uri=input_uri, dry_run=dry_run)
+      counter += 1
+      if counter % batch == 0:
+        utils.run_pending_tasks(tjsp1i_download, pendings, input_uri=input_uri, dry_run=dry_run, max_workers=max_workers)
+        pendings=[]
+    utils.run_pending_tasks(tjsp1i_download, pendings, input_uri=input_uri, dry_run=dry_run, max_workers=max_workers)
     startDate = startDate.add(months=1)
