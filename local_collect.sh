e8c7a01a43bb55e4296031aa77cb68a22e6f3458
Caio Donalisio <caio.donalisio@inspira.legal>
2023-03-17 11:32:28 -0300

    fix: bash script fix

--- local_collect.sh
+++ local_collect.sh
@@ -1,8 +1,10 @@
-for y in {2021..2021}; do
+COURT="$1"
+
+for y in {2022..2023}; do
   for m in {1..12}; do
     start_date=$(date -d "$y-$m-01" +"%Y-%m-%d")
     end_date=$(date -d "$y-$m-$(cal $m $y | awk 'NF {DAYS = $NF}; END {print DAYS}')" +"%Y-%m-%d")
-    echo $start_date $end_date
-    python -m app.commands tjsc --start-date $start_date --end-date $end_date --output-uri gs://inspira-production-buckets-tjsc
+    echo $COURT $start_date $end_date
+    python -m app.commands $COURT --start-date $start_date --end-date $end_date --output-uri gs://inspira-production-buckets-$COURT
   done
 done
